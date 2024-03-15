cf97f72b932bc303b4d4030f1d3ae70281f423a4
Av3z <willyanf17@hotmail.com>
2024-03-14 23:04:45 -0300

    feat: create a custom text field

---
+++ lib/src/components/text_field_custom.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TextFieldCustom extends StatelessWidget {
+  final bool obscureText;
+  final TextEditingController controller;
+  final String hintText;
+
+  const TextFieldCustom(
+      {super.key,
+      required this.obscureText,
+      required this.hintText,
+      required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      controller: controller,
+      decoration: InputDecoration(
+          hintText: hintText,
+          filled: true,
+          fillColor: Colors.grey[400],
+          hintStyle: const TextStyle(color: Colors.white),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade200)),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white))),
+    );
+  }
+}
