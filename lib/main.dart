f97176820f7fe965fb5cdd2ecb510585527cf701
Av3z <willyanf17@hotmail.com>
2024-04-04 10:08:38 -0300

    config: add function for wait firebase start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/firebase_options.dart';
 import 'package:flutter_chat/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
