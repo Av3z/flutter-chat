6d021b597832b5fb2c04c3170d578f23028558f0
Av3z <willyanf17@hotmail.com>
2024-04-07 23:02:45 -0300

    chore: add title to home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,6 +5,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home'),
+      ),
+    );
   }
 }
