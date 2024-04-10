d6b0c0444cc73d26b04b2db1acdf48277d94c5a1
Av3z <willyanf17@hotmail.com>
2024-04-09 22:50:50 -0300

    feat: add button for logout to home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/src/services/auth/auth_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void signOut() {
+    final authService = AuthService();
+
+    authService.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Home'),
+        actions: [
+          IconButton(
+              onPressed: signOut, icon: const Icon(Icons.logout_outlined))
+        ],
       ),
     );
   }
