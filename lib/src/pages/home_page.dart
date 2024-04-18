c5fe3a6ed7b00f5f596e782469b264d2c738c77b
Av3z <willyanf17@hotmail.com>
2024-04-18 11:51:48 -0300

    feat: list contacts

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/src/pages/chat_page.dart';
 import 'package:flutter_chat/src/services/auth/auth_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   void signOut() {
     final authService = AuthService();
 
@@ -26,6 +31,39 @@ class _HomePageState extends State<HomePage> {
               onPressed: signOut, icon: const Icon(Icons.logout_outlined))
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('error');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('loading...');
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map<Widget>((doc) => _buildUserListItem(doc))
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['email']),
+      );
+    } else {
+      return Container();
+    }
   }
 }
